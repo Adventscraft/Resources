f617d12d11a7335bcecdb8811db00d04fa3e7288
Jonas W <hdgamer1404Jonas@gmail.com>
2023-11-21 11:43:11 +0000

    i

---
+++ resources/src/main/java/de/adventskalender/resources/App.java
@@ -0,0 +1,13 @@
+package de.adventskalender.resources;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
